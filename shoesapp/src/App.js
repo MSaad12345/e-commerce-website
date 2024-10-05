5bb4f7ac822db6b54acd6a1f1e2891901dca44e3
Muhammad Saad <www.ms808080@gmail.com>
2024-10-04 22:47:17 -0700

    add all file

---
+++ shoesapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './compunent/navbar';
+import Home from './compunent/home';
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
